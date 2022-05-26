870218f738666dff6e3327b1c5d2a1154e9db5c8
Manuel Klappacher <manuel.kl900@gmail.com>
2022-05-26 09:54:01 +0200

    changed to read the bytes

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,15 @@ mod scanner;
 
 use std::io;
 use std::fs::File;
-use std::io::BufReader;
-use std::io::BufRead;
+use std::io::Read;
 
 fn main() -> io::Result<()> {
-    let f = File::open("test.md")?;
-    let file = BufReader::new(&f);
-    for line in file.lines() {
-        let l = line.unwrap();
-        scanner::scan_token(l);
-    }
+    let mut f = File::open("test.md")?;
+    let mut buffer = Vec::new();
+
+    // read up to 10 bytes
+    let _n = f.read_to_end(&mut buffer)?;
+    scanner::scan_token(&buffer);
+    println!("The bytes: {:?}", &buffer);
     Ok(())
 }
--- src/scanner.rs
+++ src/scanner.rs
@@ -37,15 +37,15 @@ struct Text {
     text: String,
 }
 
-pub(crate) fn scan_token(buffer: String) {
+pub(crate) fn scan_token(buffer: &Vec<u8>) {
     let mut token: TokenType = TokenType::None;
     let mut level: u8 = 1;
     let mut text: String = "".to_string();
 
-    for c in buffer.chars() {
+    for c in buffer {
         match c {
-            '#' => {
-                // Heading
+            35 => {
+                // Heading #
                 match token {
                     TokenType::None => {
                         token = TokenType::Heading;
@@ -59,82 +59,86 @@ pub(crate) fn scan_token(buffer: String) {
                     _ => {}
                 }
             }
-            '=' => {
-                // Alternative Heading level 1 or Text
+            61 => {
+                // Alternative Heading level 1 or Text ---
                 match token {
                     TokenType::None => {
                         token = TokenType::AlternativeHeadingOrText;
                         level = 1;
-                        text.push(c);
+                        text.push(*c as char);
                     }
                     TokenType::AlternativeHeadingOrText => {
-                        text.push(c);
+                        text.push(*c as char);
                     }
                     _ => {
                         token = TokenType::Text;
-                        text.push(c);
+                        text.push(*c as char);
                     }
                 }
             }
-            '-' => {
-                // Alternative Heading level 2 or Text
+            45 => {
+                // Alternative Heading level 2 or Text ==
                 match token {
                     TokenType::None => {
                         token = TokenType::AlternativeHeadingOrText;
                         level = 2;
-                        text.push(c);
+                        text.push(*c as char);
                     }
                     TokenType::AlternativeHeadingOrText => {
-                        text.push(c);
+                        text.push(*c as char);
                     }
                     _ => {
                         token = TokenType::Text;
-                        text.push(c);
+                        text.push(*c as char);
                     }
                 }
             }
-            ' ' => { // Space
+            32 => { // Space
                  //ignore
+            }
+            10 => { // Newline \r
+                match token {
+                    TokenType::Heading => {
+                        let header = Heading {
+                            level,
+                            text: text.clone(),
+                        };
+                        print!("{}", header.text);
+                        print!("{}", header.level);
+                        print!("{}", "  Heading  ");
+                    }
+                    TokenType::AlternativeHeadingOrText => {
+                        let alternative_header = AlternativeHeading {
+                            level,
+                            text: text.clone(),
+                        };
+                        print!("{}", alternative_header.text);
+                        print!("{}", alternative_header.level);
+                        print!("{}", "  AlternativeHeadingOrText  ");
+                    }
+                    TokenType::Text => {
+                        let text = Text { text: text.clone() };
+                        print!("{}", text.text);
+                        print!("{}", "  Text  ");
+                    }
+                    _ => {}
+                }
+                token = TokenType::None;
+                level = 1;
+                text = "".to_string();
             }
             _ => {
                 // Text
                 match token {
                     TokenType::None => {
                         token = TokenType::Text;
-                        text.push(c);
+                        text.push(*c as char);
                     }
                     _ => {
-                        text.push(c);
+                        text.push(*c as char);
                     }
                 }
             }
         }
-    }
-
-    match token {
-        TokenType::Heading => {
-            let header = Heading {
-                level,
-                text: text.clone(),
-            };
-            print!("{}", header.text);
-            print!("{}", header.level);
-            print!("{}", "  Heading  ");
-        }
-        TokenType::AlternativeHeadingOrText => {
-            let alternative_header = AlternativeHeading {
-                level,
-                text: text.clone(),
-            };
-            print!("{}", alternative_header.text);
-            print!("{}", alternative_header.level);
-            print!("{}", "  AlternativeHeadingOrText  ");
-        }
-        TokenType::Text => {
-            let text = Text { text: text.clone() };
-            print!("{}", text.text);
-            print!("{}", "  Text  ");
-        }
-        _ => {}
     }
 }
